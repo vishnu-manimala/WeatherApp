43b530ce8bb04c90f0acc303281a97ed0bf27a66
vishnuManimala <vishnumanimala8133@gmail.com>
2024-03-16 06:48:20 +0530

    setting state

---
+++ src/app/store/app.action.ts
@@ -0,0 +1,7 @@
+import { createAction, props } from "@ngrx/store";
+import { UsersResponse } from "../models/app.model";
+
+export const loginInitialized = createAction('[login] loginInitialized',props<{username:string, password:string}>());
+export const loginSuccess = createAction('[login] loginSuccess',props<{user:UsersResponse}>());
+export const loginFailed  = createAction('[login] loginFailed');
+export const logout = createAction('logout')
